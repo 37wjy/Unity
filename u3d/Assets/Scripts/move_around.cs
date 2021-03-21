78e9902cdced8f613ee53145243d738938a34b19
37wjy <37wjy@sina.com>
2021-03-21 17:56:44 +0800

    updaye

--- u3d/Assets/Scripts/move_around.cs
+++ u3d/Assets/Scripts/move_around.cs
@@ -7,6 +7,10 @@ public class move_around : MonoBehaviour
     public GameObject target;		//目标中心
     public float speed = 0.5f;			//旋转速度
 
+    void OnEnable(){
+        
+    }
+
 
     void Update()
     {
