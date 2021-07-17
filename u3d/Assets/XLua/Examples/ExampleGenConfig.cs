df7932038da1a83ca56e2eced1ccdf685596a6d8
37wjy <37wjy@sina.com>
2021-07-17 21:10:04 +0800

    update

--- u3d/Assets/XLua/Examples/ExampleGenConfig.cs
+++ u3d/Assets/XLua/Examples/ExampleGenConfig.cs
@@ -43,12 +43,13 @@ public static class ExampleGenConfig
                 typeof(ParticleSystem),
                 typeof(SkinnedMeshRenderer),
                 typeof(Renderer),
-                typeof(WWW),
+                //typeof(WWW),
                 typeof(Light),
                 typeof(Mathf),
                 typeof(System.Collections.Generic.List<int>),
                 typeof(Action<string>),
-                typeof(UnityEngine.Debug)
+                typeof(UnityEngine.Debug),
+                typeof(UnityEngine.Networking.UnityWebRequest)
             };
 
     //C#静态调用Lua的配置（包括事件的原型），仅可以配delegate，interface
