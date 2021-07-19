3199077c466ce81bc86cfe142a1db78553de69f7
37wjy <37wjy@sina.com>
2021-07-19 12:09:14 +0800

    update

---
+++ Assets/Scripts/10022/move_around.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_around : MonoBehaviour
+{
+    public GameObject target;		//目标中心
+    public float speed = 0.5f;			//旋转速度
+
+    void OnEnable(){
+        
+    }
+
+
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, Vector3.forward, speed);
+    }
+}
---
+++ Assets/Scripts/10022/params interface.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paramsinterface : MonoBehaviour
+{
+    public List<AnimationCurve> animationCurves;
+    public Vector3[] points_v3;
+    public Vector2[] points_v2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
